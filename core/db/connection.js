e0777185b73e5812f779b9823210eb33ef83110d
Emanuele Basso <bassoemanuele1704@gmail.com>
2019-06-21 16:25:40 +0200

    Add connection to database

---
+++ core/db/connection.js
@@ -0,0 +1,17 @@
+const logger = require('loglevel')
+const mongoose = require('mongoose')
+const config = require('../../config/db')
+
+const option = {
+    user: config.user,
+    pass: config.password,
+    dbName: config.db_name,
+    useNewUrlParser: true
+}
+
+mongoose.connect('mongodb://' + config.host + ':' + config.port, option, (error) => {
+    if(error) 
+        logger.error('Connection to database refused!')
+})
+
+module.exports = mongoose.connection
