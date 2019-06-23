57e8427931edd9d686c3159e635d8b669ef4088e
andrew-sa <andreamogavero.sa@gmail.com>
2019-06-23 11:11:44 +0200

    Selectboxs interation has been completed

--- public/scripts/italyrents-js/neighbourhoods.js
+++ public/scripts/italyrents-js/neighbourhoods.js
@@ -1,45 +1,69 @@
-var bergamo = [];
-var bologna = [];
-var firenze = [];
-var milano = [];
-var napoli = [];
-var roma = [];
-var venezia = [];
+var bergamo = ['Adrara San Martino', 'Adrara San Rocco', "Albano Sant'Alessandro", 'Albino', 'Algua', 'Almè', 'Almenno San Bartolomeo', 'Almenno San Salvatore', 'Alzano Lombardo', 'Ambivere', 'Antegnate', 'Arcene', 'Ardesio', "Arzago d'Adda", 'Averara', 'Aviatico', 'Azzano San Paolo', 'Azzone', 'Bagnatica', 'Barbata', 'Bariano', 'Barzana', 'Bedulita', 'Berbenno', 'Bergamo', 'Berzo San Fermo', 'Bianzano', 'Blello', 'Bolgare', 'Boltiere', 'Bonate Sopra', 'Bonate Sotto', 'Borgo di Terzo', 'Bossico', 'Bottanuco', 'Bracca', 'Branzi', 'Brembate', 'Brembate di Sopra', "Brignano Gera d'Adda", 'Brumano', 'Brusaporto', 'Calcinate', 'Calcio', "Calusco d'Adda", 'Calvenzano', 'Camerata Cornello', "Canonica d'Adda", 'Capizzone', 'Capriate San Gervasio', 'Caprino Bergamasco', 'Caravaggio', 'Carobbio degli Angeli', 'Carona', 'Carvico', 'Casazza', "Casirate d'Adda", 'Casnigo', 'Cassiglio', 'Castelli Calepio', 'Castel Rozzone', 'Castione della Presolana', 'Castro', 'Cavernago', "Cazzano Sant'Andrea", 'Cenate Sopra', 'Cenate Sotto', 'Cene', 'Cerete', "Chignolo d'Isola", 'Chiuduno', 'Cisano Bergamasco',
+'Ciserano', 'Cividate al Piano', 'Clusone', 'Colere', 'Cologno al Serio', 'Colzate', 'Comun Nuovo', 'Corna Imagna', 'Cornalba', 'Cortenuova', 'Costa di Mezzate', 'Costa Serina', 'Costa Valle Imagna', 'Costa Volpino', 'Covo', 'Credaro', 'Curno', 'Cusio', 'Dalmine', 'Dossena', 'Endine Gaiano', 'Entratico', "Fara Gera d'Adda", 'Fara Olivana con Sola', 'Filago', 'Fino del Monte', 'Fiorano al Serio', 'Fontanella', 'Fonteno', 'Foppolo', 'Foresto Sparso', 'Fornovo San Giovanni',
+'Fuipiano Valle Imagna', 'Gandellino', 'Gandino', 'Gandosso', 'Gaverina Terme', 'Gazzaniga', 'Ghisalba', 'Gorlago', 'Gorle', 'Gorno', 'Grassobbio', 'Gromo', 'Grone', 'Grumello del Monte', 'Isola di Fondra', 'Isso', 'Lallio', 'Leffe', 'Lenna', 'Levate', 'Locatello', 'Lovere', 'Lurano', 'Luzzana', 'Madone', 'Mapello',
+'Martinengo', 'Medolago', 'Mezzoldo', "Misano di Gera d'Adda", "Moio de' Calvi", 'Monasterolo del Castello', 'Montello', 'Morengo', 'Mornico al Serio', 'Mozzanica', 'Mozzo', 'Nembro', 'Olmo al Brembo', 'Oltre il Colle', 'Oltressenda Alta', 'Oneta', 'Onore', 'Orio al Serio', 'Ornica', 'Osio Sopra', 'Osio Sotto', 'Pagazzano', 'Paladina', 'Palazzago', 'Palosco', 'Parre', 'Parzanica', 'Pedrengo', 'Peia', 'Pianico', 'Piario', 'Piazza Brembana', 'Piazzatorre', 'Piazzolo', 'Pognano', 'Ponte Nossa', 'Ponteranica', 'Ponte San Pietro', 'Pontida', 'Pontirolo Nuovo', 'Pradalunga', 'Predore', 'Premolo', 'Presezzo', 'Pumenengo', 'Ranica', 'Ranzanico', 'Riva di Solto', 'Rogno', 'Romano di Lombardia', 'Roncobello', 'Roncola', "Rota d'Imagna", 'Rovetta', 'San Giovanni Bianco', "San Paolo d'Argon", 'San Pellegrino Terme', 'Santa Brigida', "Sant'Omobono Terme", 'Sarnico', 'Scanzorosciate', 'Schilpario', 'Sedrina', 'Selvino', 'Seriate', 'Serina', 'Solto Collina', 'Solza', 'Songavazzo', 'Sorisole', 'Sotto il Monte Giovanni XXIII', 'Sovere', 'Spinone al Lago', 'Spirano', 'Stezzano', 'Strozza', 'Suisio', 'Taleggio', 'Tavernola Bergamasca', 'Telgate', "Terno d'Isola", 'Torre Boldone', "Torre de' Roveri", 'Torre Pallavicina', 'Trescore Balneario', 'Treviglio', 'Treviolo', 'Ubiale Clanezzo', 'Urgnano', 'Valbondione', 'Val Brembilla', 'Valbrembo', 'Valgoglio', 'Valleve', 'Valnegra', 'Valtorta', 'Vedeseta', 'Verdellino', 'Verdello', 'Vertova', 'Viadanica', 'Vigano San Martino', 'Vigolo', "Villa d'Adda", "Villa d'Almè", 'Villa di Serio', "Villa d'Ogna", 'Villongo', 'Vilminore di Scalve', 'Zandobbio', 'Zanica', 'Zogno'];
+var bologna = ['Borgo Panigale - Reno', 'Navile', 'Porto - Saragozza', 'San Donato - San Vitale', 'Santo Stefano', 'Savena'];
+var firenze = ['Campo di Marte', 'Centro Storico', 'Gavinana Galluzzo', 'Isolotto Legnaia', 'Rifredi'];
+var milano = ['ADRIANO', 'AFFORI', 'BAGGIO', 'BANDE NERE', 'BARONA', 'BICOCCA', 'BOVISA', 'BOVISASCA', 'BRERA', 'BRUZZANO', 'BUENOS AIRES - VENEZIA', 'CANTALUPA', 'CASCINA TRIULZA - EXPO', 'CENTRALE', 'CHIARAVALLE', "CITTA' STUDI", 'COMASINA', 'CORSICA', 'DE ANGELI - MONTE ROSA', 'DERGANO', 'DUOMO', 'EX OM - MORIVIONE', 'FARINI', 'FIGINO', 'FORZE ARMATE', 'GALLARATESE', 'GARIBALDI REPUBBLICA', 'GHISOLFA', 'GIAMBELLINO', 'GIARDINI PORTA VENEZIA', 'GRATOSOGLIO - TICINELLO', 'GRECO', 'GUASTALLA', 'ISOLA', 'LAMBRATE', 'LODI - CORVETTO', 'LORENTEGGIO', 'LORETO', 'MACIACHINI - MAGGIOLINA', 'MAGENTA - S. VITTORE', 'MAGGIORE - MUSOCCO', 'MECENATE', 'MUGGIANO', 'NAVIGLI', "NIGUARDA - CA' GRANDA", 'ORTOMERCATO', 'PADOVA', 'PAGANO', 'PARCO AGRICOLO SUD', 'PARCO BOSCO IN CITTÀ', 'PARCO DEI NAVIGLI', 'PARCO DELLE ABBAZIE', 'PARCO FORLANINI - ORTICA', 'PARCO LAMBRO - CIMIANO', "PARCO MONLUE' - PONTE LAMBRO", 'PARCO NORD', 'PARCO SEMPIONE', 'PORTA ROMANA', 'PORTELLO', 'QT 8', 'QUARTO CAGNINO', 'QUARTO OGGIARO', 'QUINTO ROMANO', 'QUINTOSOLE', 'RIPAMONTI', 'ROGOREDO', 'RONCHETTO DELLE RANE', 'RONCHETTO SUL NAVIGLIO', 'SACCO', 'SARPI', 'SCALO ROMANA', 'S. CRISTOFORO', 'SELINUNTE', 'S. SIRO', 'STADERA', 'STEPHENSON', 'TIBALDI', 'TICINESE', 'TORTONA', 'TRENNO', 'TRE TORRI', 'TRIULZO SUPERIORE', 'UMBRIA - MOLISE', 'VIALE MONZA', 'VIGENTINA', 'VILLAPIZZONE', 'WASHINGTON', 'XXII MARZO'];
+var napoli = ['Arenella', 'Avvocata', 'Bagnoli', 'Barra', 'Chiaia', 'Chiaiano', 'Fuorigrotta', 'Mercato', 'Miano', 'Montecalvario', 'Pendino', 'Pianura', 'Piscinola', 'Poggioreale', 'Ponticelli', 'Porto', 'Posillipo', "San Carlo all'Arena", 'San Ferdinando', 'San Giovanni a Teduccio', 'San Giuseppe', 'San Lorenzo', 'San Pietro a Patierno', 'Scampia', 'Secondigliano', 'Soccavo', 'Stella', 'Vicaria', 'Vomero', 'Zona Industriale'];
+var roma = ['I Centro Storico', 'III Monte Sacro', 'II Parioli/Nomentano', 'IV Tiburtina', 'IX Eur', 'VIII Appia Antica', 'VII San Giovanni/Cinecittà', 'VI Roma delle Torri', 'V Prenestino/Centocelle', 'XI Arvalia/Portuense', 'XIII Aurelia', 'XII Monte Verde', 'XIV Monte Mario', 'X Ostia/Acilia', 'XV Cassia/Flaminia'];
+var venezia = ['Alberoni', 'Burano', 'Cannaregio', 'Castello', 'Dorsoduro', 'Giudecca', 'Isola Buel del Lovo', 'Isola Campalto', 'Isola Carbonera', "Isola Cason Grassabo'", "Isola Cason vecchio di Grassabo'", 'Isola delle Tresse', 'Isola di Tessera', 'Isola ex Batteria Fisolo', 'Isola ex Forte S.Angelo della Polve', 'Isola ex Ridotto di Crevan', 'Isola La Cura', 'Isola La Grazia', 'Isola La Salina', 'Isola Lazzaretto vecchio', 'Isola Madonna del Monte', 'Isola Ottagono Alberoni', "Isola Ottagono di Ca' Roman", 'Isola Ottagono San Pietro', 'Isola Peschieretta della Granzona', 'Isola Poveglia', 'Isola Sacca Sessola', 'Isola San Clemente', 'Isola San Francesco del Deserto', 'Isola San Giacomo in Paludo', 'Isola San Giorgio', 'Isola San Giorgio in Alga', 'Isola San Lazzaro degli Armeni', 'Isola San Michele', 'Isola San Secondo', 'Isola San Servolo', 'Isola Santa Cristina', 'Isola Santo Spirito', 'Lazzaretto Nuovo', 'Lido', 'Malamocco', 'Mazzorbetto', 'Mazzorbo', 'Murano', 'Pellestrina', 'Sacca Fisola', 'Sacca San Biagio', 'San Marco', 'San Pietro in Volta', 'San Polo', 'Santa Croce', 'Santa Maria del Mare', "Sant'Elena", "Sant'Erasmo", 'Torcello', 'Tronchetto', 'Vignole', 'Aeroporto', 'Altobello', 'Asseggiano', 'Bissuola', 'Borgoforte', 'Borgo Pezzana', "Ca' Brentelle", "Ca' Emiliani", 'Campalto', 'Campalto Bagaron', 'Campalto CEP', 'Campalto Cimitero', 'Campalto Gobbi', 'Capitello', 'Carpenedo', "Ca' Sabbioni", 'Case Dosa', "Ca' Solaro", 'Centro Commerciale Auchan', 'Centro Commerciale Panorama', 'Chirignago', 'Cipressina', 'Dese', 'Favaro', 'Favaro Cimitero', 'Favaro Col San Martino', 'Ferrarese', 'Gatta - Bonduà', 'Gazzera', 'Giustizia', 'La Favorita', 'Macello', 'Maguolo', 'Malcontenta', 'Marghera', 'Marghera Catene', 'Marghera Zona Industriale', 'Marocco Terraglio', 'Marocco Zelarino', 'Molino Ronchin', 'Piave 1860', "Pra' Secco", 'Quartiere Pertini', 'San Lorenzo XXV Aprile', 'Santa Barbara', 'Sardi (M.5 - Q11)', 'Sardi (M.5 - Q.12)', 'Scaramuzza', "Taru'", 'Tessera', 'Torre Antica', 'Trivignano', 'Villabona', 'Villaggio San Marco', 'Villaggio Sartori', 'Zelarino', 'Zona Commerciale via Torino'];
 
-function loadNeighbourhoods(citiesSelectBox)
+function loadNeighbourhoods()
 {
-    var value = citiesSelectBox.value;
-    var neighbourhoodsSelectBox = (document.getElementsByName('neighbourhood'))[0];
+    /*var value = citiesSelectBox.value;
+    var neighbourhoodsSelectBox = (document.getElementsByName('neighbourhood'))[0];*/
+    var citiesSelectBox = $('#selectCities');
+    var value = citiesSelectBox.val();
+    var neighbourhoodsSelectBox = $('#selectNeighbourhoods');
     switch (value)
     {
         case 'Bergamo':
-            neighbourhoodsSelectBox.disabled = false;
+            addNeighbourhoods(neighbourhoodsSelectBox, bergamo);
+            neighbourhoodsSelectBox.prop('disabled', false);
             break;
         case 'Bologna':
+            addNeighbourhoods(neighbourhoodsSelectBox, bologna);
+            neighbourhoodsSelectBox.prop('disabled', false);
             break;
         case 'Firenze':
-                break;
+            addNeighbourhoods(neighbourhoodsSelectBox, firenze);
+            neighbourhoodsSelectBox.prop('disabled', false);
+            break;
         case 'Milano':
+            addNeighbourhoods(neighbourhoodsSelectBox, milano);
+            neighbourhoodsSelectBox.prop('disabled', false);
             break;
         case 'Napoli':
+            addNeighbourhoods(neighbourhoodsSelectBox, napoli);
+            neighbourhoodsSelectBox.prop('disabled', false);
             break;
         case 'Roma':
+            addNeighbourhoods(neighbourhoodsSelectBox, roma);
+            neighbourhoodsSelectBox.prop('disabled', false);
             break;
         case 'Venezia':
+            addNeighbourhoods(neighbourhoodsSelectBox, roma);
+            neighbourhoodsSelectBox.prop('disabled', false);
             break;
         default:
-            neighbourhoodsSelectBox.disabled = true;
-            neighbourhoodsSelectBox.
+            neighbourhoodsSelectBox.prop('disabled', true);
+            removeNeighbourhoods(neighbourhoodsSelectBox);
             break;
     }
 }
 
 function addNeighbourhoods(box, values)
 {
-
+    removeNeighbourhoods(box);
+    box.append('<option value="All" selected>All</option>');
+    for (var i = 0; i < values.length; i++)
+    {
+        box.append('<option value="' + values[i] + '">' + values[i] + '</option>');
+    }
 }
 
 function removeNeighbourhoods(box)
 {
-    
+    box.empty();
 }
