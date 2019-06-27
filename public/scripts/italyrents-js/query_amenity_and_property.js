1dfe3c40396d943075899dd06754243f16fa3de9
Emanuele Basso <bassoemanuele1704@gmail.com>
2019-06-27 12:04:54 +0200

    Front end Query Amenity & Property finished

---
+++ public/scripts/italyrents-js/query_amenity_and_property.js
@@ -0,0 +1,40 @@
+function retrievalData(){
+    var request = {}
+
+    var amenity = $('#amenity_combo option:selected').prop('value')
+    request['amenity'] = amenity
+
+    var property = $('#property_combo option:selected').prop('value')
+    request['property'] = property
+
+    var price = $('#price_combo option:selected').prop('value')
+    request['price'] = price
+
+    var rating = $('#rating_combo option:selected').prop('value')
+    request['rating'] = rating
+
+    var mq = $('#mq_combo option:selected').prop('value')
+    request['mq'] = mq
+
+    startLoader()
+
+    $.ajax({
+        method: 'POST',
+        url: '/ItalyRents/squareMetresClassesQuery',
+        data: request,
+        dataType: 'json'
+    })
+    .done(function( data ) {
+        //console.log(data)
+
+        showData(data)
+                
+        stopLoader()
+    })
+}
+
+function showData(data){
+    $('#result_price').text(data['price'])
+    $('#result_rating').text(data['rating'])
+    $('#result_mq').text(data['mq'])
+}
