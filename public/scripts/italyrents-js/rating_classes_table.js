f4d78b77ac6afd702211edba6b2ed01ce261fa0c
andrew-sa <andreamogavero.sa@gmail.com>
2019-06-27 16:12:51 +0200

    Rating Classes table with price has been completed

--- public/scripts/italyrents-js/rating_classes_table.js
+++ public/scripts/italyrents-js/rating_classes_table.js
@@ -1,8 +1,17 @@
 function fixSecondCol()
 {
-    var width = $('.first-col').width();
-    var paddingLeft = $('.first-col').css('padding-left');
-    var paddingRight = $('.first-col').css('padding-right');
-    
-    $('.second-col').css('left');
+    var size = [];
+    var regex = /[^0-9.]/g;
+    var width = 0;
+    size.push($('.first-col').width());
+    size.push($('.first-col').css('padding-left'));
+    size.push($('.first-col').css('padding-right'));
+    // console.log(width);
+    for (var e of size)
+    {
+        var str = String(e).replace(regex, '');
+        width += parseFloat(str);
+        // console.log(width);
+    }
+    $('.second-col').css('left', width);
 }
