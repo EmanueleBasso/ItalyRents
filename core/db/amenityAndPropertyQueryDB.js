67ff2bdae1cb32d80a123393d8cde86faa985590
andrew-sa <andreamogavero.sa@gmail.com>
2019-06-27 12:29:08 +0200

    Server side of Amenity and Property query has been completed

---
+++ core/db/amenityAndPropertyQueryDB.js
@@ -0,0 +1,91 @@
+const logger = require('loglevel')
+const connection = require('./connection')
+
+module.exports = async function(property, amenity, price, rating, mq, sendResponse, res){
+    await Promise.all([
+        query('Bergamo', property, amenity, price, rating, mq), 
+        query('Bologna', property, amenity, price, rating, mq),
+        query('Firenze', property, amenity, price, rating, mq), 
+        query('Milano', property, amenity, price, rating, mq), 
+        query('Napoli', property, amenity, price, rating, mq), 
+        query('Roma', property, amenity, price, rating, mq), 
+        query('Venezia', property, amenity, price, rating, mq)]).then(async (responses) => {
+            sendResponse(responses, res, price, rating, mq)
+        })
+}
+
+async function query(city, property, amenity, price, rating, mq){
+    await sleep(1200)    //  Testing
+
+    return new Promise( (resolve, reject) => {
+        (async () => {
+            connection.db.collection('listings' + city, function(err, collection){
+                if(err)
+                    logger.error(err)
+
+                var pipeline = []
+
+                // Property
+                var pType = {
+                    $match: {
+                        property_type: property
+                    }
+                }
+                pipeline.push(pType)
+
+                // Amenity
+                var aType = {
+                    $match: {
+                    }
+                }
+                aType['$match']['amenities.' + amenity] = true;
+                pipeline.push(aType)
+
+                // Group
+                var group = {
+                    $group: {
+                        _id: null
+                    }
+                }
+                group['$group']['price'] = {}
+                group['$group']['price']['$' + price] = '$price'
+                group['$group']['rating'] = {}
+                group['$group']['rating']['$' + rating] = {
+                    $divide: [{
+                        $add: [
+                            '$review_scores_accuracy', '$review_scores_cleanliness', '$review_scores_checkin', '$review_scores_communication', '$review_scores_location'
+                        ]
+                    }, 5]
+                }
+                group['$group']['mq'] = {}
+                group['$group']['mq']['$' + mq] = '$square_metres'
+                pipeline.push(group)
+
+                console.log(JSON.stringify(pipeline, null, 4))
+                collection.aggregate(pipeline).toArray(function(err, docs){
+                    if(err) 
+                        logger.error(err)
+                    console.log(docs[0])
+                    if(docs[0] == undefined){
+                        resolve(null)
+                    }else{
+                        delete docs[0]._id
+    
+                        resolve(docs[0])
+                    }
+                })
+            })
+        })()
+    })
+}
+
+
+
+
+
+//  Testing
+query("Milano","Bed and breakfast","TV","avg", "min", "min")
+//  Testing
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
--- core/query/amenityAndPropertyQuery.js
+++ core/query/amenityAndPropertyQuery.js
@@ -1,5 +1,182 @@
 const amenityAndPropertyQueryDB = require('../db/amenityAndPropertyQueryDB')
 
 module.exports = async function (req, res){
+    var property = req.body.property
+    var amenity = req.body.amenity
+    var price = req.body.price
+    var rating = req.body.rating
+    var mq = req.body.mq
+
+    amenityAndPropertyQueryDB(property, amenity, price, rating, mq, sendResponse, res)
+}
+
+function sendResponse(result, res, price, rating, mq){
+    var priceValue = NaN;
+    var ratingValue = NaN;
+    var mqValue = NaN;
+    var priceCount = 0;
+    var ratingCount = 0;
+    var mqCount = 0;
+    for (var obj of result)
+    {
+        if (obj != null)
+        {
+            var p = obj.price;
+            var r = obj.rating;
+            var m = obj.mq;
+            if (p != null)
+            {
+                if (isNaN(priceValue))
+                {
+                    priceValue = p
+                    if (price == 'avg')
+                    {
+                        priceCount++;
+                    }
+                }
+                else
+                {
+                    if (price == 'avg')
+                    {
+                        priceValue += p
+                        priceCount++;
+                    }
+                    else if (price == 'min')
+                    {
+                        if (p < priceValue)
+                        {
+                            priceValue = p
+                        }
+                    }
+                    else if (price == 'max')
+                    {
+                        if (p > priceValue)
+                        {
+                            priceValue = p
+                        }
+                    }
+                }
+            }
+            if (r != null)
+            {
+                if (isNaN(ratingValue))
+                {
+                    ratingValue = r
+                    if (rating == 'avg')
+                    {
+                        ratingCount++;
+                    }
+                }
+                else
+                {
+                    if (rating == 'avg')
+                    {
+                        ratingValue += r
+                        ratingCount++;
+                    }
+                    else if (rating == 'min')
+                    {
+                        if (r < ratingValue)
+                        {
+                            ratingValue = r
+                        }
+                    }
+                    else if (rating == 'max')
+                    {
+                        if (r > ratingValue)
+                        {
+                            ratingValue = r
+                        }
+                    }
+                }
+            }
+            if (m != null)
+            {
+                if (isNaN(mqValue))
+                {
+                    mqValue = m
+                    if (mq == 'avg')
+                    {
+                        mqCount++;
+                    }
+                }
+                else
+                {
+                    if (mq == 'avg')
+                    {
+                        mqValue += m
+                        mqCount++;
+                    }
+                    else if (mq == 'min')
+                    {
+                        if (m < mqValue)
+                        {
+                            mqValue = m
+                        }
+                    }
+                    else if (mq == 'max')
+                    {
+                        if (m > mqValue)
+                        {
+                            mqValue = m
+                        }
+                    }
+                }
+            }               
+        }
+    }
     
+    if (!isNaN(priceValue))
+    {
+        if (price == 'avg')
+        {
+            if (priceCount != 0)
+            {
+                priceValue /= priceCount
+            }
+        }
+        priceValue = parseFloat((Math.round(((priceValue + 0.00001) * 100)) / 100).toFixed(2))
+    }
+    else
+    {
+        priceValue = 'N/A'
+    }
+
+    if (!isNaN(ratingValue))
+    {
+        if (rating == 'avg')
+        {
+            if (ratingCount != 0)
+            {
+                ratingValue /= ratingCount
+            }
+        }
+        ratingValue = parseFloat((Math.round(((ratingValue + 0.00001) * 100)) / 100).toFixed(2))
+    }
+    else
+    {
+        ratingValue = 'N/A'
+    }
+
+    if (!isNaN(mqValue))
+    {
+        if (mq == 'avg')
+        {
+            if (mqCount != 0)
+            {
+                mqValue /= mqCount
+            }
+        }
+        mqValue = parseFloat((Math.round(((mqValue + 0.00001) * 100)) / 100).toFixed(2))
+    }
+    else
+    {
+        mqValue = 'N/A'
+    }
+
+    var resultObj = {};
+    resultObj['price'] = priceValue
+    resultObj['rating'] = ratingValue
+    resultObj['mq'] = mqValue
+    res.json(resultObj)
 }
